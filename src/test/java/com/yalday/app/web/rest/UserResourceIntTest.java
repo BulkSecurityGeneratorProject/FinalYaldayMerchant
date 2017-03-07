7f3aa5754a5f7887c636014f5e654a0c772a459b
chriskiernan87 <chriskiernan87@googlemail.com>
2017-03-07 18:51:20 +0000

    Additional tests

--- src/test/java/com/yalday/app/web/rest/UserResourceIntTest.java
+++ src/test/java/com/yalday/app/web/rest/UserResourceIntTest.java
@@ -1,9 +1,7 @@
 package com.yalday.app.web.rest;
 
 import com.yalday.app.FinalYaldayMerchantApp;
-import com.yalday.app.domain.Merchant;
 import com.yalday.app.domain.User;
-import com.yalday.app.domain.dto.MerchantDTO;
 import com.yalday.app.domain.dto.UserDTO;
 import com.yalday.app.repository.UserRepository;
 import com.yalday.app.repository.search.UserSearchRepository;
@@ -13,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -28,7 +27,12 @@ import java.time.Instant;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -134,5 +138,114 @@ public class UserResourceIntTest {
 
         List<User> users = userRepository.findAll();
         assertThat(users).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllUsers() throws Exception {
+        // Initialize the database
+        User saved = userRepository.saveAndFlush(user.toUser());
+
+        // Get all the users
+        restUserMockMvc.perform(get("/api/users?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(saved.getId().intValue())))
+            .andExpect(jsonPath("$.[*].email").value(hasItem(DEFAULT_EMAIL)));
+    }
+
+    @Test
+    @Transactional
+    public void getUser() throws Exception {
+        // Initialize the database
+        User saved = userRepository.saveAndFlush(user.toUser());
+
+        // Get the user
+        restUserMockMvc.perform(get("/api/users/{id}", saved.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(saved.getId().intValue()))
+            .andExpect(jsonPath("$.email").value(DEFAULT_EMAIL.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingUser() throws Exception {
+        // Get the user
+        restUserMockMvc.perform(get("/api/users/{id}", Long.MAX_VALUE))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateUser() throws Exception {
+        // Initialize the database
+        User saved = userRepository.saveAndFlush(user.toUser());
+        userSearchRepository.save(saved);
+        int databaseSizeBeforeUpdate = userRepository.findAll().size();
+
+        // Update the user
+        UserDTO updatedUser = UserDTO.builder()
+                .email(UPDATED_EMAIL)
+                .build();
+
+        Thread.sleep(50); // so the last edited time is different
+
+        Timestamp updateTime = Timestamp.from(Instant.now(Clock.systemDefaultZone()));
+        restUserMockMvc.perform(post("/api/users/{id}", saved.getId())
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(updatedUser)))
+                .andExpect(status().isOk());
+
+        // Validate the User in the database
+        List<User> users = userRepository.findAll();
+        assertThat(users).hasSize(databaseSizeBeforeUpdate);
+        User testUser = users.get(users.size() - 1);
+        assertThat(testUser.getEmail()).isEqualTo(UPDATED_EMAIL);
+        assertThat(testUser.getDateCreated()).isEqualTo(saved.getDateCreated());
+        assertThat(testUser.getLastEdited()).isAfter(updateTime);
+
+        // Validate the User in ElasticSearch
+        User userEs = userSearchRepository.findOne(testUser.getId());
+        assertThat(userEs).isEqualToComparingFieldByField(testUser);
+    }
+
+    @Test
+    @Transactional
+    public void deleteUser() throws Exception {
+        // Initialize the database
+        User saved = userRepository.saveAndFlush(user.toUser());
+        userSearchRepository.save(saved);
+        int databaseSizeBeforeDelete = userRepository.findAll().size();
+
+        // Get the user
+        restUserMockMvc.perform(delete("/api/users/{id}", saved.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate ElasticSearch is empty
+        boolean userExistsInEs = userSearchRepository.exists(saved.getId());
+        assertThat(userExistsInEs).isFalse();
+
+        // Validate the database is empty
+        List<User> users = userRepository.findAll();
+        assertThat(users).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void searchUser() throws Exception {
+        // Initialize the database
+        User saved = userRepository.saveAndFlush(user.toUser());
+        userSearchRepository.save(saved);
+
+        // Search the user
+        restUserMockMvc.perform(get("/api/_search/users?query=id:" + saved.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(saved.getId().intValue())))
+            .andExpect(jsonPath("$.[*].email").value(hasItem(DEFAULT_EMAIL)))
+            .andExpect(jsonPath("$.[*].dateCreated").exists())
+            .andExpect(jsonPath("$.[*].lastEdited").exists());
     }
 }
